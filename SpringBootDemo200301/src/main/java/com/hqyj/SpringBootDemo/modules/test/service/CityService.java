077c0619c8924323cbd95e0f743747eb4aab75d8
YY <YY@WINDOWS-OFIL371>
2020-06-05 20:18:57 +0800

    "2020-06-05"

--- SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/test/service/CityService.java
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/test/service/CityService.java
@@ -2,7 +2,9 @@ package com.hqyj.SpringBootDemo.modules.test.service;
 
 import java.util.List;
 
+
 import com.github.pagehelper.PageInfo;
+import com.hqyj.SpringBootDemo.modules.common.vo.Result;
 import com.hqyj.SpringBootDemo.modules.common.vo.SearchVo;
 import com.hqyj.SpringBootDemo.modules.test.entity.City;
 
@@ -15,4 +17,12 @@ public interface CityService {
 	PageInfo<City> getCitiesByPage(int currentPage, int pageSize, int countryId);
 	
 	PageInfo<City> getCitiesBySearchVo(SearchVo searchVo);
+	
+	Result<City> insetCity(City city);
+	
+	Result<City> updateCity(City city);
+	
+	Result<Object> deleteCity(int cityId);
+	
+	Object migrateCitiesByCountryId(int countryId);
 }
--- SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/test/service/impl/CityServiceImpl.java
+++ SpringBootDemo200301/src/main/java/com/hqyj/SpringBootDemo/modules/test/service/impl/CityServiceImpl.java
@@ -1,24 +1,31 @@
 package com.hqyj.SpringBootDemo.modules.test.service.impl;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.hqyj.SpringBootDemo.modules.common.vo.Result;
+import com.hqyj.SpringBootDemo.modules.common.vo.Result.ResultStatus;
 import com.hqyj.SpringBootDemo.modules.common.vo.SearchVo;
 import com.hqyj.SpringBootDemo.modules.test.dao.CityDao;
 import com.hqyj.SpringBootDemo.modules.test.entity.City;
 import com.hqyj.SpringBootDemo.modules.test.service.CityService;
+import com.hqyj.SpringBootDemo.utils.RedisUtils;
 
 @Service
 public class CityServiceImpl implements CityService {
 	
 	@Autowired
 	private CityDao cityDao;
+	@Autowired
+	private RedisUtils redisUtils;
 
 	@Override
 	public List<City> getCitiesByCountryId(int countryId) {
@@ -48,4 +55,32 @@ public class CityServiceImpl implements CityService {
 				.orElse(Collections.emptyList()));
 	}
 
+	@Override
+	public Result<City> insetCity(City city) {
+		city.setDateCreated(new Date());
+		cityDao.insetCity(city);
+		return new Result<City>(ResultStatus.SUCCESS.status, "Insert success", city);
+	}
+
+	@Override
+	@Transactional
+	public Result<City> updateCity(City city) {
+		cityDao.updateCity(city);
+//		int a = 1 / 0;
+		return new Result<City>(ResultStatus.SUCCESS.status, "Update success.", city);
+	}
+
+	@Override
+	public Result<Object> deleteCity(int cityId) {
+		cityDao.deleteCity(cityId);
+		return new Result<Object>(ResultStatus.SUCCESS.status, "Delete success.");
+	}
+
+	@Override
+	public Object migrateCitiesByCountryId(int countryId) {
+		List<City> cities = getCitiesByCountryId(countryId);
+		redisUtils.set("cities", cities);
+		return redisUtils.get("cities");
+	}
+	
 }
